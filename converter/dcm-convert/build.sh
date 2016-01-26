07711a0ce5bb8dd0c708d881d97bdd271a12f012
Michael Perry <lmperry@stanford.edu>
2016-01-26 13:20:54 -0800

    Adds dcm-convert gear with flywheel v0 spec.

---
+++ converter/dcm-convert/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Builds the dcm-convert container. 
+# The container can be exported using the export.sh script
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+docker build --no-cache --tag scitran/dcm-convert $DIR
