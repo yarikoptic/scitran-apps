8355245fb5ad69a1e4295b0394fb65100c953a9e
Michael Perry <lmperry@stanford.edu>
2016-04-04 14:42:28 -0700

    Fix input arg handling to allow names with spaces.
    Fixes #12 for dcm-convert and qa-report-fmri apps.

--- qa/qa-report-fmri/export.sh
+++ qa/qa-report-fmri/export.sh
@@ -2,7 +2,7 @@
 # Exports the container in the cwd.
 # The container can be exported once it's started with 
 
-version=0.1.1
+version=0.1.2
 outname=qa-report-fmri-$version.tar
 container=qa-report-fmri
 image=scitran/qa-report-fmri
