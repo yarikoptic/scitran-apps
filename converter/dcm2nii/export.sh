53cb73b6cccf50f656f365476bbf65ea31297457
Michael Perry <lmperry@stanford.edu>
2016-04-23 17:43:43 -0700

    v0.0.3: Don't reorient & ignore hidden input files
    Increase the minimum reorient matrix value to prevent reorientation to cannonical space. This prevents the writing out of another nifti file prefixed with 'o'. In addition, t1w scans will no longer be cropped. Now, save for the case of a Localizer scan, a single nifti file should be generated.
    
    Also fixed file handling in the run script to ignore hidden files and directories.

--- converter/dcm2nii/export.sh
+++ converter/dcm2nii/export.sh
@@ -2,7 +2,7 @@
 # Exports the container in the cwd.
 # The container can be exported once it's started with
 
-version=0.0.2
+version=0.0.3
 outname=dcm2nii-$version.tar
 container=dcm2nii
 image=scitran/dcm2nii
