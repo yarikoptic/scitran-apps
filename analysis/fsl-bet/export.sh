32270498273fa5d8d842c5e09411390684b27d1b
Michael Perry <lmperry@stanford.edu>
2016-05-10 09:29:46 -0700

    Update run scripts to configure ENV at runtime.
    When gears are executed via the engine the ENV as configured by the
    Dockerfile is not preserved, thus the ENV must be configured by the run
    script at runtime.

--- analysis/fsl-bet/export.sh
+++ analysis/fsl-bet/export.sh
@@ -2,7 +2,7 @@
 # Exports the container in the cwd.
 # The container can be exported once it's started with
 
-version=0.0.2
+version=0.0.3
 outname=fsl-bet-$version.tar
 container=fsl-bet
 image=scitran/fsl-bet
