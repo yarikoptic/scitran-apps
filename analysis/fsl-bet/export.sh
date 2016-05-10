9e2bddd5aff8210d008a7f9c4e1d59e0571fab0d
Michael Perry <lmperry@stanford.edu>
2016-05-10 16:21:48 -0700

    Update fsl-bet, fsl-fast gears to output '.metadata.json'
    The engine requires that the '.metadata.json' file be present for the
    upload to commence.

--- analysis/fsl-bet/export.sh
+++ analysis/fsl-bet/export.sh
@@ -2,7 +2,7 @@
 # Exports the container in the cwd.
 # The container can be exported once it's started with
 
-version=0.0.3
+version=0.0.4
 outname=fsl-bet-$version.tar
 container=fsl-bet
 image=scitran/fsl-bet
