4f941e5b385f1c4bfae9e0bf8bf66892aa7813d4
Michael Perry <lmperry@stanford.edu>
2016-06-22 22:23:29 -0700

    v0.0.8: handle flat zip files. use updated meta utility. be quieter.

--- converter/dcm2nii/build.sh
+++ converter/dcm2nii/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Builds the dcm-convert container.
+# Builds the gear.
 # The container can be exported using the export.sh script
 
+GEAR=scitran/dcm2niix
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build --no-cache --tag scitran/dcm2nii $DIR
+docker build --no-cache --tag $GEAR $DIR
