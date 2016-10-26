33c94735e827919ed1d057fec6b91f083b732b30
Michael Perry <lmperry@stanford.edu>
2016-10-26 09:27:21 -0700

    Delete apps that have been moved to scitran-apps.

--- qa/qa-dtiprep/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Build container within this context
-CONTAINER=scitran/qa-dtiprep
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-docker build --no-cache --tag $CONTAINER $DIR
-
