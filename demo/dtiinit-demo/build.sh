5d01a2299f0ac68a68c334cbaed5a96a39351eb4
Michael Perry <lmperry@stanford.edu>
2016-10-27 10:51:42 -0700

    Reorganization: remove demo gears

--- demo/dtiinit-demo/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Builds the brain-extraction container.
-# The container can be exported using the export.sh script
-
-GEAR=scitran/dtiinit-demo
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build --no-cache --tag $GEAR $DIR
--- demo/reconall-demo/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# The container can be exported using the export.sh script
-GEAR=scitran/reconall-demo
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build --no-cache --tag $GEAR $DIR
